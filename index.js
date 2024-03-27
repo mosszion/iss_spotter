26d477967c81397d1ae0e8777da1f2032566f4db
Mussie <musdzion@gmail.com>
2024-03-27 12:19:13 -0400

    index file holding functions

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+//import fetchMyIp file
+const { fetchMyIP } = require('./iss');
+// setting our Ip address fetching function
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+});
+
 
