1b748c103269fdf359cc42021389f6b1b6d8eae3
Mussie <musdzion@gmail.com>
2024-03-27 15:01:48 -0400

    flyover function imported to index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //import fetchMyIp file
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIp } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
+
 // setting our Ip address fetching function
 
 // fetchMyIP((error, ip) => {
@@ -12,12 +14,21 @@ const { fetchCoordsByIp } = require('./iss');
 //   console.log('It worked! Returned IP:' , ip);
 // });
 
-fetchCoordsByIp("", (error, coordinates) => {
+//fetching fetchCoordsByIp function
+
+// fetchCoordsByIp("2607:fea8:a4c7:8900:604e:ab49:e11e:1e9", (error, coordinates) => {
+// if(error) {
+//   console.log ("It didn't work!", error);
+//   return;
+// }
+// console.log("It worked! Returned coordinates:", coordinates)
+// })
+fetchISSFlyOverTimes({ latitude: 43.653226, longitude: -79.3831843 }, (error, passTimes) => {
 if(error) {
   console.log ("It didn't work!", error);
   return;
 }
-console.log("It worked! Returned coordinates:", coordinates)
+console.log("It worked! Returned flyover times:", passTimes);
 })
 
 
